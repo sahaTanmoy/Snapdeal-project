b9fda9c12b21eb5b12024f5801a839768dbdf396
sahaTanmoy <tanmoy.saha@bitcanny.com>
2022-02-19 23:23:44 +0530

    login validation more checking added

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -18,14 +18,14 @@ function LoginForm(props1) {
     // {userData, fetchUsers}=props1
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [entry, setEntry] = useState({email: '', password: ''})
+    const [entry, setEntry] = useState({ email: '', password: '' })
     const navigate = useNavigate()
     const [users, setUsers] = useState([])
     const [errMsg, setErrMsg] = useState('')
     const [cart, setCart] = useState([])
-    const [errMsg2, setErrMsg2] = useState('')
-    const [emailErr,setEmailErr]= useState('')
-    const [passErr,setPassErr]= useState('')
+    const [uservalidationMsg, setUservalidationMsg] = useState("")
+    const [emailErr, setEmailErr] = useState('')
+    const [passErr, setPassErr] = useState('')
 
     useEffect(() => {
         // props1.fetchUsers()
@@ -51,47 +51,72 @@ function LoginForm(props1) {
     //     dispatch(fetchUserCart(userid))
     // }, [])
 
-    
+    // const individualuser=[]
 
 
-    const individualuser = users.filter(user => ((user.email === entry.email) && (user.password === entry.password)))
-    console.log("Individualuser1:", individualuser[0]);
+    // const individualuser = users.filter(user => ((user.email === entry.email) && (user.password === entry.password)))
+    // console.log("Individualuser1:", individualuser);
 
     // useEffect(() => {
     //     dispatch(fetchUserCart(individualuser[0].id))
     // }, [])
 
 
-    individualuser.length ? (dispatch(isAuthenticated(true, individualuser[0]))) : dispatch(isAuthenticated(false, {}))
-    individualuser.length ? (dispatch(fetchUserCart(individualuser[0].id))) : dispatch(isAuthenticated(false, {}))
+    // individualuser.length ? (dispatch(isAuthenticated(true, individualuser[0]))) : dispatch(isAuthenticated(false, {}))
+    // individualuser.length ? (dispatch(fetchUserCart(individualuser[0].id))) : dispatch(isAuthenticated(false, {}))
 
-    console.log(212,entry);
+    console.log(212, entry);
 
     const handleSubmitForm = (e) => {
         e.preventDefault();
-        if(email.length===0 || (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))){
+        if (email.length === 0 || (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))) {
             setEmailErr('**Enter a valid Email');
+            entry.email = '';
             // setEntry({ ...entry,email: '' });
-            entry.email= '';
-            console.log(2121,"ok1");
+            // console.log(2121, "ok1");
         }
         else {
             setEmailErr('');
-            
+            entry.email = email
             // setEntry({ ...entry,email: email });
-            entry.email=email
-            console.log(2121,"ok2");
+            // console.log(2121, "ok2");
         }
         if (password.length < 6) {
-            // alert(`Password should contain 6 characters.your password is only ${password.length} character long`)
             setPassErr(`**Password should contain 6 characters`);
+            entry.password = ''
             // setEntry({ ...entry,password: '' })
-            entry.password=''
-        } 
+        }
         else {
             setPassErr('');
+            entry.password = password
             // setEntry({ ...entry,password: password })
-            entry.password=password
+        }
+        if ((email.length !== 0
+            && (email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))
+        ) && (password.length > 5)) {
+            // console.log("block executing");
+            const individualuser = users.filter(user => ((user.email === entry.email) && (user.password === entry.password)))
+            console.log("Individualuser1:", individualuser);
+            if (users.length === 0) {
+                if(errMsg.length===0){
+                    setUservalidationMsg(`Authitication failed. Please try again.`)
+                }
+                else{
+                setUservalidationMsg(`Authitication failed due to ${errMsg}. Please refresh the page and try Again.`)
+                }
+            } else {
+                if (individualuser.length === 0) {
+                    setUservalidationMsg("Enter Right Credentials")
+                    dispatch(isAuthenticated(false, {}))
+                }
+                else {
+                    individualuser.map(user => {
+                        dispatch(isAuthenticated(true, user))
+                        dispatch(fetchUserCart(user.id))
+                    })
+                    navigate(`/`, { replace: true })
+                }
+            }
         }
     }
 
@@ -100,98 +125,69 @@ function LoginForm(props1) {
     return (
         <div className='loginbackcontainer'>
             <Row>
-            {/* <>
-            <Container>
-                <Form className='logincls' onSubmit={handleSubmitForm}>
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder='Enter Email Address' 
-                    value={email} onChange={e=>setEmail(e.target.value.toLowerCase())}></Form.Control>
+                <div >
+                    <Container>
+                        <div className='logformcontainer'>
+                            <Row>
 
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder='Enter Password' 
-                    value={password} onChange={e=>setPassword(e.target.value)}></Form.Control>
+                                <Col sm={5}>
+                                    <Row>
+                                        <Col sm={2}><GrLocation className='logformicon1' size={50} /></Col>
+                                        <Col sm>
+                                            <h6>MANAGE YOUR ORDERS</h6>
+                                            <p>
+                                                Track orders, manage cancellations & returns.
+                                            </p>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col sm={2}><RiFileList3Line className='logformicon2' size={50} /></Col>
+                                        <Col sm>
+                                            <h6>SHORTLIST ITEMS YOU LOVE</h6>
+                                            <p>
+                                                Keep items you love on a watchlist.
+                                            </p>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col sm={2}><IoIosNotificationsOutline className='logformicon3' size={50} /></Col>
+                                        <Col sm>
+                                            <h6>AWESOME OFFERS UPDATES FOR YOU</h6>
+                                            <p>
+                                                Be first to know about great offers and save.
+                                            </p>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                <Col sm>
+                                    <div className='loginform'>
+                                        <h4>Login On Snapdeal</h4>
+                                        {/* <Form onSubmit={handleSubmitForm} > */}
+                                        <Form onSubmit={handleSubmitForm}>
+                                            <Form.Label className='mb-2'>Email Address</Form.Label>
+                                            <Form.Control className='mb-1' type="email" placeholder='Enter Email Address'
+                                                value={email} onChange={e => setEmail(e.target.value.toLowerCase())}></Form.Control>
+                                            <div className='frminputerr'>{emailErr}</div>
 
-                    <Button variant='danger' type='submit'>Sign In</Button>
-                </Form>
-            </Container>
-            </> */}
-            <div >
-                <Container>
-                    <div className='logformcontainer'>
-                        <Row>
+                                            <Form.Label className='mb-2'>Password</Form.Label>
+                                            <Form.Control className='mb-1' type="password" placeholder='Enter Password'
+                                                value={password} onChange={e => setPassword(e.target.value)}></Form.Control>
+                                            <div className='frminputerr'>{passErr}</div>
+                                            <div className="d-grid gap-2">
+                                                <Button className='mb-3 mt-2' variant='danger' type='submit' >Sign In</Button>
+                                            </div>
+                                            <div className='frminputerr'>
+                                                {uservalidationMsg}
+                                            </div>
+                                        </Form>
+                                    </div>
+                                </Col>
 
-                            <Col sm={5}>
-                                <Row>
-                                    <Col sm={2}><GrLocation className='logformicon1' size={50} /></Col>
-                                    <Col sm>
-                                        <h6>MANAGE YOUR ORDERS</h6>
-                                        <p>
-                                            Track orders, manage cancellations & returns.
-                                        </p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col sm={2}><RiFileList3Line className='logformicon2' size={50} /></Col>
-                                    <Col sm>
-                                        <h6>SHORTLIST ITEMS YOU LOVE</h6>
-                                        <p>
-                                            Keep items you love on a watchlist.
-                                        </p>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col sm={2}><IoIosNotificationsOutline className='logformicon3' size={50} /></Col>
-                                    <Col sm>
-                                        <h6>AWESOME OFFERS UPDATES FOR YOU</h6>
-                                        <p>
-                                            Be first to know about great offers and save.
-                                        </p>
-                                    </Col>
-                                </Row>
-                            </Col>
-                            <Col sm>
-                                <div className='loginform'>
-                                    <h4>Login On Snapdeal</h4>
-                                    {/* <Form onSubmit={handleSubmitForm} > */}
-                                    <Form onSubmit={handleSubmitForm}>
-                                        <Form.Label className='mb-2'>Email Address</Form.Label>
-                                        <Form.Control className='mb-1' type="email" placeholder='Enter Email Address'
-                                            value={email} onChange={e => setEmail(e.target.value.toLowerCase())}></Form.Control>
-                                        <div className='frminputerr'>{emailErr}</div>
-
-                                        <Form.Label className='mb-2'>Password</Form.Label>
-                                        <Form.Control className='mb-1' type="password" placeholder='Enter Password'
-                                            value={password} onChange={e => setPassword(e.target.value)}></Form.Control>
-                                        <div className='frminputerr'>{passErr}</div>
-                                        <div className="d-grid gap-2">
-                                            <Button className='mb-3 mt-2' variant='danger' type='submit' >Sign In</Button>
-                                        </div>
-
-                                    </Form>
-                                </div>
-                            </Col>
-
-                        </Row>
-                    </div>
-                </Container>
-            </div>
+                            </Row>
+                        </div>
+                    </Container>
+                </div>
             </Row>
-            <div>
-                <Container>
-                    {/* <Demo />  */}
-                    {
-                        // userData && userData.users &&
-                        // userData.users.filter(user=>((user.email === email)&&(user.password === password)))
-                        individualuser.length ?
-                            (individualuser.map(user =>
-                                // navigate(`/user/${user.id}/carts`, { replace: true }))
-                                navigate(`/`, { replace: true }))
-                            ) : (<h1>Enter right credentials</h1>)
-                    }
-
-                </Container>
-            </div>
-
         </div>
     )
 }
